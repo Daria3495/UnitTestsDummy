df944ace0a6e7a2395bf5ca5552ad44803daf42f
Daria Kornieva <darya.aleksandrovna95@gmail.com>
2024-04-06 01:06:10 +0300

    finished with unit-tests

--- src/test/java/junittests/JUnitRunner.java
+++ src/test/java/junittests/JUnitRunner.java
@@ -1,32 +1,86 @@
 package junittests;
 
 import classes.Calculator;
+import classes.EvenOddChecker;
+import classes.Rectangle;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
+
 public class JUnitRunner {
     Calculator calculator = new Calculator();
+    EvenOddChecker evenOddChecker = new EvenOddChecker();
+
+    double length = 2;
+
+    double width = 10;
+    Rectangle rectangle = new Rectangle(length, width);
+
 
     @Test
     public void checkAddMethod() {
-        assertEquals(33, calculator.add(20, 13));
+        assertEquals("The ADD method returned incorrect SUM", 33, calculator.add(20, 13));
     }
 
     @Test
     public void checkSubtractMethod() {
-        assertEquals(40, calculator.subtract(45,5));
+        assertEquals("The SUBTRACT method returned incorrect SUM", 40, calculator.subtract(45, 5));
     }
 
     @Test
     public void checkMultiplyMethod() {
-        assertEquals(60, calculator.multiply(5,12));
+        assertEquals("The MULTIPLY method returned incorrect SUM", 60, calculator.multiply(5, 12));
     }
 
     @Test
     public void checkDivideMethod() {
-        assertEquals(6, calculator.divide(18,3));
-//        assertEquals(6, calculator.divide(18,0));
+        assertEquals("The DIVIDE method returned incorrect SUM", 6, calculator.divide(18, 3));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void dividingByZero() {
+        assertEquals("Dividing by zero does not returned error", calculator.divide(7, 0));
+    }
+
+    @Test
+    public void checkDivisionWithoutRemainder() {
+        assertTrue("The number divided with reminder, expected EVEN", evenOddChecker.check(8));
+    }
+
+    @Test
+    public void checkDivisionWithRemainder() {
+        assertFalse("The number divided without reminder, expected ODD", evenOddChecker.check(9));
+    }
+
+    @Test
+    public void checkObjectConstructor() {
+        assertEquals("Length is not the same as in object", length, rectangle.getLength(), 0.001);
+        assertEquals("Width is not the same as in object", width, rectangle.getWidth(), 0.001);
+    }
+
+    @Test
+    public void checkSettingNewLength() {
+
+        double newLength = 7.0;
+
+        rectangle.setLength(newLength);
+        assertEquals("Length is not set with new parameter", newLength, rectangle.getLength(), 0.001);
+    }
+
+    @Test
+    public void checkSettingNewWidth() {
+
+        double newWidth = 5.0;
+
+        rectangle.setWidth(newWidth);
+        assertEquals("Width is not set with new parameter", newWidth, rectangle.getWidth(), 0.001);
+    }
+
+    @Test
+    public void checkAreaForObject() {
+        assertEquals("The area of the rectangle is not as expected", 20.0, rectangle.getArea(), 0.001);
     }
 
 }
--- src/test/java/testng/TestNgRunner.java
+++ src/test/java/testng/TestNgRunner.java
@@ -1,6 +1,8 @@
 package testng;
 
 import classes.Calculator;
+import classes.EvenOddChecker;
+import classes.Rectangle;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,32 +11,87 @@ public class TestNgRunner {
 
     Calculator calculator = new Calculator();
 
+    double length = 6;
+
+    double width = 8;
+    Rectangle rectangle = new Rectangle(length, width);
+
     @Test
     public void verifyAddMethod() {
-
-        Assert.assertEquals(calculator.add(2, 3), 5);
-
+        Assert.assertEquals(calculator.add(2, 3), 5,
+                "The ADD method returned incorrect SUM");
     }
 
     @Test
     public void verifySubtractMethod() {
-
-        Assert.assertEquals(calculator.subtract(12, 9), 3);
-
+        Assert.assertEquals(calculator.subtract(12, 9), 3,
+                "The SUBTRACT method returned incorrect SUM");
     }
 
     @Test
     public void verifyMultiplyMethod() {
-
-        Assert.assertEquals(calculator.multiply(7, 15), 105);
-
+        Assert.assertEquals(calculator.multiply(7, 15), 105,
+                "The MULTIPLY method returned incorrect SUM");
     }
 
     @Test
     public void verifyDivideMethod() {
+        Assert.assertEquals(calculator.divide(90, 9), 10,
+                "The DIVIDE method returned incorrect SUM");
+    }
 
-        Assert.assertEquals(calculator.divide(90, 9), 10);
+    @Test (expectedExceptions = ArithmeticException.class)
+    public void dividingByZero() {
+        Assert.assertEquals(calculator.divide(8, 0),
+                "Dividing by zero does not returned error");
+    }
 
+    @Test
+    public void checkRemainderOfZeroIsEven() {
+        Assert.assertTrue(EvenOddChecker.check(12),
+                "The number divided with reminder, expected EVEN");
+    }
+
+    @Test
+    public void checkRemainderOfZeroIsOdd() {
+        Assert.assertFalse(EvenOddChecker.check(13),
+                "The number divided without reminder, expected ODD");
+    }
+
+    @Test
+    public void checkCreationOfRectangle() {
+        Assert.assertEquals(rectangle.getWidth(), width,0.001,
+                "Width is not the same as in object");
+        Assert.assertEquals(rectangle.getLength(), length,0.001,
+                "Length is not the same as in object");
+    }
+
+    @Test
+    public void checkSettingNewLengthToRectangle() {
+
+        double newLength = 13.0;
+
+        rectangle.setLength(newLength);
+
+        Assert.assertEquals(rectangle.getLength(), newLength,0.001,
+                "Width is not the same as in object");
+    }
+
+    @Test
+    public void checkSettingNewWidthToRectangle() {
+
+        double newWidth = 5.0;
+
+        rectangle.setWidth(newWidth);
+
+        Assert.assertEquals(rectangle.getWidth(), newWidth,0.001,
+                "Width is not the same as in object");
+    }
+
+    @Test
+    public void checkAreaForRectangle() {
+        Assert.assertEquals(rectangle.getArea(), 48.00, 0.001,
+                "The area of the rectangle is not as expected");
     }
 
 }
